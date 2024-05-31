259953feee2cf4f8be91261425567d37699af610
Hemant Srivastava <143774541+Hemant5104@users.noreply.github.com>
2024-05-31 16:24:32 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  // Get the member
+  var member = getMember();
+  // Get the member's skills
+  var skills = member.skills;
+  // Return the member's skills
+  return skills;
+}
